dd33ef2ba319852c9da27c2122e02697e9c13632
AMAH911 <andrewhenry906@gmail.com>
2023-05-02 20:10:13 -0400

    created current weather component

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { IcurrentWeather } from '../icurrent-weather';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+current: IcurrentWeather
+
+constructor(){
+  this.current = {
+    city: 'Bethesda',
+    country: 'Us',
+    date: new Date(),
+    image:'assets/img/sunny.svg',
+    description:'sunny',
+    tempurature:72
+  } as IcurrentWeather
+}
+
+ngOnInit(): void {
+
+}
+}
